ee1d477033570d0ca81df0a7a7e99d88596420c8
Max M <meshstyles@users.noreply.github.com>
2021-06-15 03:46:57 +0200

    remove unneded code

--- scrapers/twitter_media_scrape/run.sh
+++ scrapers/twitter_media_scrape/run.sh
@@ -4,13 +4,11 @@ username="$1"
 max="$2"
 link="https://en.whotwi.com/${username}/tweets/media?&page="
 userAgent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36 OPR/76.0.4017.177"
-df="./failed-download-$username.file"
 jsonfile="/tmp/index-$username.json"
 
 mkdir "$username"
 cd "$username"
 
-> "$df"
 
 for I in `seq 1 $max`
 do
@@ -22,11 +20,8 @@ do
         # generate link for image
         imageLink=$(jq -r ".[$k].children[0].children[0].src" "$jsonfile" | sed 's/\/\//https:\/\//');
         # echo "$imageLink"
-        # postLink=$(jq -r ".[$k].children[0].href" "$jsonfile" | sed 's/\/\//https:\/\//');
-        # echo $postLink
         
         # try downloading image and log on fail
-        # wget -c "$imageLink" --user-agent="${userAgent}" || jq -r ".[$k].children[0].href" "$jsonfile" | sed 's/\/\//https:\/\//' >> "$df"
         wget -c "$imageLink" --user-agent="${userAgent}" || echo "$imageLink" >> 0.txt
         
     # this causes a replace of t though it should replace tabs but it's not required because of jq
