ef129fa229fac0eebe1d8260b4915665d83866b8
Max M <meshstyles@users.noreply.github.com>
2021-11-22 13:03:04 +0100

    Update run.sh

--- media-dl/run.sh
+++ media-dl/run.sh
@@ -22,27 +22,9 @@ if [[ "${1}" =~ ^(https:\/\/www.tubitv.com\/series\/|https:\/\/tubitv.com\/serie
 fi
 
 if [[ "${1}" =~ ^(https:\/\/www.netzkino.de|https:\/\/netzkino.de) ]];then
-    
-    film_id=$(echo "${1}" | sed 's/https:\/\/www.netzkino.de\/filme\///' | sed 's/\///')
-    pmdext=$(curl "https://data.netzkino.de/netzkino/graphql?operationName=MovieDetails&variables=%7B%22movieId%22%3A%22$film_id%22%2C%22externalId%22%3A%22$film_id%22%7D&extensions=%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22sha256Hash%22%3A%220c851312c3b1c70912c6164633dcbc632401d59f1f61f69754bb24a4993da54d%22%7D%7D" \
-        -H 'Connection: keep-alive' \
-        -H 'sec-ch-ua: "Chromium";v="94", "Google Chrome";v="94", ";Not A Brand";v="99"' \
-        -H 'accept: */*' \
-        -H 'content-type: application/json' \
-        -H 'sec-ch-ua-mobile: ?0' \
-        -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36' \
-        -H 'sec-ch-ua-platform: "Windows"' \
-        -H 'Origin: https://www.netzkino.de' \
-        -H 'Sec-Fetch-Site: same-site' \
-        -H 'Sec-Fetch-Mode: cors' \
-        -H 'Sec-Fetch-Dest: empty' \
-        -H 'Referer: https://www.netzkino.de/' \
-        -H 'Accept-Language: de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7,zh-CN;q=0.6,zh;q=0.5' \
-        --compressed | jq -r '.data.movieBySlug.nodes[0].videoSource.pmdUrl')
-    if [[ "$pmdext" == "null"* ]];then 
-        echo "the link could not be retrieved"
-        exit 1
-    fi
+    page=$(curl "${1}")
+    pmdext=$(echo "$page" | pup 'script#__NEXT_DATA__ text{}' | jq '.' | grep 'pmdUrl' | cut -d '"' -f 4)
+    echo $pmdext
     wget -c "https://pmd.netzkino-seite.netzkino.de/$pmdext" || exit 1
     exit 0
 fi 
